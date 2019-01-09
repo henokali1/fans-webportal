197298a1c3254c8b666d32bf8f9b6a4a61f958ae
henokali1 <henokali1@gmail.com>
2019-01-09 16:41:00 +0400

    Monthly check urls and view added.

--- cns_monthly_check/urls.py
+++ cns_monthly_check/urls.py
@@ -4,5 +4,21 @@ from . import views
 
 urlpatterns = [
     path('', views.cns_monthly_check, name='cns_monthly_check.html'),
+    path('aftn_and_cadas/', views.aftn_and_cadas, name='aftn_and_cadas.html'),
+    path('atis/', views.atis, name='atis.html'),
+    path('ats_radar_display_system/', views.ats_radar_display_system, name='ats_radar_display_system.html'),
+    path('aws_rwy_11_29_ceilometer_29/', views.aws_rwy_11_29_ceilometer_29, name='aws_rwy_11_29_ceilometer_29.html'),
+    path('dgvox_recording_system/', views.dgvox_recording_system, name='dgvox_recording_system.html'),
+    path('dvor/', views.dvor, name='dvor.html'),
+    path('equipment_room_servers_pm/', views.equipment_room_servers_pm, name='equipment_room_servers_pm.html'),
+    path('glidepath/', views.glidepath, name='glidepath.html'),
+    path('hp_dme/', views.hp_dme, name='hp_dme.html'),
+    path('jotron_tx_rx_vhf/', views.jotron_tx_rx_vhf, name='jotron_tx_rx_vhf.html'),
+    path('localizer/', views.localizer, name='localizer.html'),
+    path('lp_dme/', views.lp_dme, name='lp_dme.html'),
+    path('rdd/', views.rdd, name='rdd.html'),
+    path('remote_monitors_workshop/', views.remote_monitors_workshop, name='remote_monitors_workshop.html'),
+    path('schmid_report/', views.schmid_report, name='schmid_report.html'),
+    path('uhf_base_radio_vhf/', views.uhf_base_radio_vhf, name='uhf_base_radio_vhf.html'),
     path('vhf_ground_to_air_radios/', views.vhf_ground_to_air_radios, name='vhf_ground_to_air_radios.html'),
 ]
--- cns_monthly_check/views.py
+++ cns_monthly_check/views.py
@@ -4,6 +4,70 @@ from django.shortcuts import render
 def cns_monthly_check(request):
     return render(request, 'cns_monthly_check/cns_monthly_check.html')
 
+# aftn_and_cadas
+def aftn_and_cadas(request):
+    return render(request, 'cns_monthly_check/aftn_and_cadas.html')
+
+# atis
+def atis(request):
+    return render(request, 'cns_monthly_check/atis.html')
+
+# ats_radar_display_system
+def ats_radar_display_system(request):
+    return render(request, 'cns_monthly_check/ats_radar_display_system.html')
+
+# ats_radar_display_system
+def aws_rwy_11_29_ceilometer_29(request):
+    return render(request, 'cns_monthly_check/aws_rwy_11_29_ceilometer_29.html')
+
+# dgvox_recording_system
+def dgvox_recording_system(request):
+    return render(request, 'cns_monthly_check/dgvox_recording_system.html')
+
+# dvor
+def dvor(request):
+    return render(request, 'cns_monthly_check/dvor.html')
+
+# equipment_room_servers_pm
+def equipment_room_servers_pm(request):
+    return render(request, 'cns_monthly_check/equipment_room_servers_pm.html')
+
+# glidepath
+def glidepath(request):
+    return render(request, 'cns_monthly_check/glidepath.html')
+
+# hp_dme
+def hp_dme(request):
+    return render(request, 'cns_monthly_check/hp_dme.html')
+
+# jotron_tx_rx_vhf
+def jotron_tx_rx_vhf(request):
+    return render(request, 'cns_monthly_check/jotron_tx_rx_vhf.html')
+
+# localizer
+def localizer(request):
+    return render(request, 'cns_monthly_check/localizer.html')
+
+# lp_dme
+def lp_dme(request):
+    return render(request, 'cns_monthly_check/lp_dme.html')
+
+# rdd
+def rdd(request):
+    return render(request, 'cns_monthly_check/rdd.html')
+
+# remote_monitors_workshop
+def remote_monitors_workshop(request):
+    return render(request, 'cns_monthly_check/remote_monitors_workshop.html')
+
+# schmid_report
+def schmid_report(request):
+    return render(request, 'cns_monthly_check/schmid_report.html')
+
+# uhf_base_radio_vhf
+def uhf_base_radio_vhf(request):
+    return render(request, 'cns_monthly_check/uhf_base_radio_vhf.html')
+
 # vhf_ground_to_air_radios
 def vhf_ground_to_air_radios(request):
     return render(request, 'cns_monthly_check/vhf_ground_to_air_radios.html')
