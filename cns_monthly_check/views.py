2d61a83c7e96cdd53a3eb895aacc51869d6c6ee6
henokali1 <henokali1@gmail.com>
2019-01-10 08:22:02 +0400

    CNS monthly check login required.

--- cns_monthly_check/views.py
+++ cns_monthly_check/views.py
@@ -1,73 +1,93 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Base URL's
+@login_required
 def cns_monthly_check(request):
     return render(request, 'cns_monthly_check/cns_monthly_check.html')
 
-# aftn_and_cadas
+# AFTN AND CADAS
+@login_required
 def aftn_and_cadas(request):
     return render(request, 'cns_monthly_check/aftn_and_cadas.html')
 
-# atis
+# ATIS
+@login_required
 def atis(request):
     return render(request, 'cns_monthly_check/atis.html')
 
-# ats_radar_display_system
+# ATS RADAR DISPLAY SYSTEM
+@login_required
 def ats_radar_display_system(request):
     return render(request, 'cns_monthly_check/ats_radar_display_system.html')
 
-# ats_radar_display_system
+# AWS RWY 11 29 CEILOMETER 29
+@login_required
 def aws_rwy_11_29_ceilometer_29(request):
     return render(request, 'cns_monthly_check/aws_rwy_11_29_ceilometer_29.html')
 
-# dgvox_recording_system
+# DGVOX RECORDING SYSTEM
+@login_required
 def dgvox_recording_system(request):
     return render(request, 'cns_monthly_check/dgvox_recording_system.html')
 
-# dvor
+# DVOR
+@login_required
 def dvor(request):
     return render(request, 'cns_monthly_check/dvor.html')
 
-# equipment_room_servers_pm
+# EQUIPMENT ROOM SERVERS PM
+@login_required
 def equipment_room_servers_pm(request):
     return render(request, 'cns_monthly_check/equipment_room_servers_pm.html')
 
-# glidepath
+# GLIDEPATH
+@login_required
 def glidepath(request):
     return render(request, 'cns_monthly_check/glidepath.html')
 
-# hp_dme
+# HP DME
+@login_required
 def hp_dme(request):
     return render(request, 'cns_monthly_check/hp_dme.html')
 
-# jotron_tx_rx_vhf
+# JOTRON TX RX VHF
+@login_required
 def jotron_tx_rx_vhf(request):
     return render(request, 'cns_monthly_check/jotron_tx_rx_vhf.html')
 
-# localizer
+# LOCALIZER
+@login_required
 def localizer(request):
     return render(request, 'cns_monthly_check/localizer.html')
 
-# lp_dme
+# LP DME
+@login_required
 def lp_dme(request):
     return render(request, 'cns_monthly_check/lp_dme.html')
 
-# rdd
+# RDD
+@login_required
 def rdd(request):
     return render(request, 'cns_monthly_check/rdd.html')
 
-# remote_monitors_workshop
+# REMOTE MONITORS WORKSHOP
+@login_required
 def remote_monitors_workshop(request):
     return render(request, 'cns_monthly_check/remote_monitors_workshop.html')
 
-# schmid_report
+# SCHMID REPORT
+@login_required
 def schmid_report(request):
     return render(request, 'cns_monthly_check/schmid_report.html')
 
-# uhf_base_radio_vhf
+# UHF BASE RADIO VHF
+@login_required
 def uhf_base_radio_vhf(request):
     return render(request, 'cns_monthly_check/uhf_base_radio_vhf.html')
 
-# vhf_ground_to_air_radios
+# VHF GROUND TO AIR RADIOS
+@login_required
 def vhf_ground_to_air_radios(request):
     return render(request, 'cns_monthly_check/vhf_ground_to_air_radios.html')
